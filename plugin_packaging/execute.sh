7ffa8125faf969eaa32908af652584e4462fd9d6
Fabian Trautsch <fabian.trautsch@informatik.uni-goettingen.de>
2017-07-14 11:26:34 +0200

    fixing bugs in execute.sh (force checkout) and update gitignore

--- plugin_packaging/execute.sh
+++ plugin_packaging/execute.sh
@@ -7,7 +7,7 @@ NEW_UUID=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
 cp -R $REPOSITORY_PATH "/dev/shm/$NEW_UUID"
 
 cd "/dev/shm/$NEW_UUID"
-git checkout $3 --quiet
+git checkout -f --quiet $3
 
 COMMAND="python3.5 $PLUGIN_PATH/main.py --input /dev/shm/$NEW_UUID --output /dev/shm/$NEW_UUID --rev $3 --url $4 --db-hostname $5 --db-port $6 --db-database $7"
 
