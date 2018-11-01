9ba88203c0cc511fe8e2678833d9db534936a428
alexander.linne <alexander.linne@stud.uni-goettingen.de>
2018-11-01 10:21:34 +0100

    disable cppcheck for SourceMeter
    SourceMeter runs cppcheck with an older C++ standard set. This generates
    a lot of errors in the cppcheck output that wouldn't occur otherwise.

--- templates/analyze_c.ps1
+++ templates/analyze_c.ps1
@@ -1,4 +1,4 @@
 $dir = $pwd.Path
 cd $results
-& "$cSourcemeter" @("-maximumThreads=4", "-projectName=$projectname", "-buildScript=build.bat",  "-resultsDir=$results" , "-runCppcheck=true", "-runMetricHunter=false", "-runFaultHunter=false", "-runDCF=true", "-externalSoftFilter=external-filter.txt")
+& "$cSourcemeter" @("-maximumThreads=4", "-projectName=$projectname", "-buildScript=build.bat",  "-resultsDir=$results" , "-runCppcheck=false", "-runMetricHunter=false", "-runFaultHunter=false", "-runDCF=true", "-externalSoftFilter=external-filter.txt")
 cd $dir
