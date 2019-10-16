3bcaca0788098839a7a2456eebbd18cd70ca4abc
atrautsch <alexander.trautsch@cs.uni-goettingen.de>
2019-10-16 16:07:12 +0200

    wget needs --quiet otherwise we get an error

--- plugin_packaging/install.sh
+++ plugin_packaging/install.sh
@@ -4,12 +4,12 @@ PLUGIN_PATH=$1
 cd $PLUGIN_PATH/external/sloccount2.26
 make
 
-cd $PLUGIN_PATH/external/sourcemeter/Java
-./installMavenWrapper.sh
+# cd $PLUGIN_PATH/external/sourcemeter/Java
+# ./installMavenWrapper.sh
 
 cd $PLUGIN_PATH/external
-wget https://github.com/smartshark/OpenStaticAnalyzer/releases/download/v4.0.0-smartshark/OpenStaticAnalyzer-4.0.0-x64-Linux.tgz
-tar -xvf OpenStaticAnalyzer-4.0.0-x64-Linux.tgz 
+wget --quiet https://github.com/smartshark/OpenStaticAnalyzer/releases/download/v4.0.0-smartshark/OpenStaticAnalyzer-4.0.0-x64-Linux.tgz
+tar -xvf OpenStaticAnalyzer-4.0.0-x64-Linux.tgz
 mv OpenStaticAnalyzer-4.0.0-x64-Linux openStaticAnalyzer
 
 cd $PLUGIN_PATH
